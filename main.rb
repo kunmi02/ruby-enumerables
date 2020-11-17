887fa6ecad282e9bd4720f04b764bb5c2c6a7034
Suyash <suyash.krishna9@gmail>
2020-11-17 17:57:00 +0400

    Add both Proc and Block to my_map

--- main.rb
+++ main.rb
@@ -24,12 +24,24 @@ module Enumerable
     end
   end
 
-  def my_map(&proc1)
+  def my_map(proc1 = nil)
     i = 0
     new_array = []
-    while i < length
-      new_array.push(proc1.call(self[i]))
-      i += 1
+    if !proc1.nil? && proc1.respond_to?(:call)
+      puts('It is Proc')
+      while i < length
+        new_array.push(proc1.call(self[i]))
+        i += 1
+      end
+    elsif block_given?
+      puts('It is Block')
+      while i < length
+        new_array.push(yield(self[i]))
+        i += 1
+      end
+    else
+      puts('It is none')
+      new_array = self
     end
     new_array
   end
@@ -48,3 +60,26 @@ end
 def multiply_els(array)
   array.my_inject { |sum, item| sum * item }
 end
+
+puts('# my_map test')
+proc1 = proc do |item|
+  item + 1
+end
+
+puts('## No Argument')
+puts([1, 2, 3].my_map)
+
+puts('## Non-Proc Argument')
+puts([1, 2, 3].my_map('Hello'))
+
+puts('## Proc Argument')
+puts([1, 2, 3].my_map(proc1))
+
+puts('## Both Proc and Block')
+puts([1, 2, 3].my_map(proc1) { |item| item + 1 })
+
+puts('## Non Proc and Block')
+puts([1, 2, 3].my_map('Hello') { |item| item + 1 })
+
+puts('## Only Block')
+puts([1, 2, 3].my_map { |item| item + 1 })
