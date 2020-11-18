c0172823578ede300252614765987f7b5c38bb5c
Suyash <suyash.krishna9@gmail>
2020-11-18 18:31:13 +0400

    Reduce lines as per rubocop requirement

--- main.rb
+++ main.rb
@@ -34,11 +34,10 @@ module Enumerable
   def my_any?(data = nil)
     if block_given?
       my_each { |item| return true if yield(item) }
-      false
     elsif data.nil?
-      my_each { |n| return true unless n.nil?}
-    elsif !data.nil? && (data.is_a? Class)
-      my_each { |n| return true if n.class == data }
+      my_each { |n| return true unless n.nil? }
+    elsif data.is_a? Class
+      my_each { |n| return true if n.instance_of?(data) }
     else
       my_each { |n| return true if n == data }
     end
@@ -83,13 +82,15 @@ module Enumerable
   def my_inject(*data)
     if block_given?
       net = data[0] ? yield(first, data[0]) : first
-      my_each_with_index { |item, index| net = yield(net, item) if index != 0 }
-    elsif data[0] && data[1]
-      net = data[0]
-      my_each { |item| net = net.send(data[1], item) }
+      drop(1).my_each { |item| net = yield(net, item) }
     elsif data[0]
-      net = first
-      my_each_with_index { |item, index| net = net.send(data[0], item) if index != 0 }
+      if data[1]
+        net = data[0]
+        my_each { |item| net = net.send(data[1], item) }
+      else
+        net = first
+        drop(1).my_each { |item| net = net.send(data[0], item) }
+      end
     end
     net
   end
@@ -98,3 +99,12 @@ end
 def multiply_els(array)
   array.my_inject { |sum, item| sum * item }
 end
+
+# 9. my_inject
+puts 'my_inject'
+puts '---------'
+p [1, 2, 3, 4].my_inject(10) { |accum, elem| accum + elem } # => 20
+p [1, 2, 3, 4].my_inject { |accum, elem| accum + elem } # => 10
+p [5, 1, 2].my_inject('+') # => 8
+p (5..10).my_inject(2, :*) # should return 302400
+p (5..10).my_inject(4) { |prod, n| prod * n } # should return 604800
