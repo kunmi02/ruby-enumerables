0f12754cd0a0f1bccd1115c6cb859340b214c135
Azeez <mrkhunmie@gmail.com>
2020-11-19 09:13:58 +0100

    my_each method to return enumerable if no block is given

--- main.rb
+++ main.rb
@@ -1,6 +1,8 @@
 # rubocop:disable Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Metrics/ModuleLength
 module Enumerable
   def my_each
+    return to_enum(:my_each) unless block_given?
+
     i = 0
     while i < to_a.length
       yield(to_a[i])
